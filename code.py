fad8bb286541f830ed3d364ede34f86f442eb342
Hoang Cuong <hoangcuong2011@gmail.com>
2017-11-28 21:49:27 -0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,13 @@
+
+b = SelectKBest(score_func=mutual_info_regression, k=2)
+X_new = b.fit_transform(X, y)
+mask = b.get_support()
+
+new_features = [] # The list of your K best features
+
+for bool, feature in zip(mask, X_valid):
+    if bool:
+        new_features.append(X_valid)
+print(new_features)
+
+#new_features should be the sames as X_new
