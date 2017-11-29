ce3b6a8e33778db2b53e420e709b852c492158dc
Hoang Cuong <hoangcuong2011@gmail.com>
2017-11-29 13:15:14 -0500

    Update code.py

--- code.py
+++ code.py
@@ -7,7 +7,7 @@ new_features = [] # The list of your K best features
 
 for bool, feature in zip(mask, X_valid):
     if bool:
-        new_features.append(X_valid)
+        new_features.append(feature)
 print(new_features)
 
 #new_features should be the sames as X_new
